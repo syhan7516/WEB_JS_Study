94b9b69d6588dc2ffee49a107a2a846a4e9b740a
syhan7516 <syhan7516@naver.com>
2022-04-23 22:27:51 +0900

    프론트 스터디 1주차 내용 & 실습 정리

---
+++ src/1주차_생활코딩/1주차_7_tagcontrol.js
@@ -0,0 +1,10 @@
+document.write("<div>7장 제어할 태그 선택</div>");
+
+// document.querySelcetor('선택자')로 제어 가능!
+// .속성."속성의 속성 = 값"으로 설정 가능!
+// .getElementById('')와querySelector의 차이!!
+// 1.전자가 1.2배 속도가 빠르지만, 지정한 선택자만 제어 가능!
+// 2.후자는 속도는 느리지만, 모든 선택자를 제어 가능!  
+
+// 실습 : document.getElementByID('ID')와 위의 차이는?
+// 실습 : night 버튼 생성 후 클릭 이벤트로 배경 색바꾸기
