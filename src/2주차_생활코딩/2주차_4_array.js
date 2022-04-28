a871682cdb4d313efa832d14163482ce1138c75a
syhan7516 <syhan7516@naver.com>
2022-04-28 23:21:31 +0900

    배열 연습

---
+++ src/2주차_생활코딩/2주차_4_array.js
@@ -0,0 +1,38 @@
+document.write("<div>4장 배열</div>");
+
+// 배열 생성
+var arr1 = [];
+var arr2 = ["shin","lee"];
+var arr3 = new Array();
+var arr4 = new Array(1,2,3,4);
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+
+// 배열 출력
+console.log(arr2[0]);
+
+// 배열 개수
+console.log(arr2.length);
+
+// 배열 내 데이터 추가
+console.log(arr2.push(arr1));
+console.log(arr2);
+
+// 배열이란?
+// 비원시 자료형으로 서로 연관된 많은 데이터를 정리하여 담아두는 곳!
+// 생성 방법 : var, const, let + 
+// 1. + 배열명 = [];
+// 2. + 배열명 = [값1,값2...];
+// 3. + 배열명 = new Array();
+// 4. + 배열명 = new Array(값1,값2,값3...);
+// 요소 접근 : 변수명[인덱스] (인덱스는 0부터 시작 주의!)
+// 배열 길이 : 배열명.length
+// 배열 내 데이터 추가 : 배열명.push(값)
+
+// 실습 1 : 자신의 개인정보를 저장할 빈 배열 만들기!
+// 실습 2 : 빈 배열에 이름, 나이, 직업유무, 먹었던 점심 메뉴, 시력을 추가해보자!
+// → 점심 메뉴는 객체로, 시력은 배열로!
+// 실습 3 : 배열 요소에 null, undefined, NaN가 추가되는지 확인해보기!
