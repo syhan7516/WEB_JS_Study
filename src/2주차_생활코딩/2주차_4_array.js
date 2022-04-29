b04a8c802ae2c35b46f16c130b5dd4931205cf62
syhan7516 <syhan7516@naver.com>
2022-04-29 14:30:40 +0900

    함수 연습

--- src/2주차_생활코딩/2주차_4_array.js
+++ src/2주차_생활코딩/2주차_4_array.js
@@ -1,25 +1,25 @@
 document.write("<div>4장 배열</div>");
 
-// 배열 생성
-var arr1 = [];
-var arr2 = ["shin","lee"];
-var arr3 = new Array();
-var arr4 = new Array(1,2,3,4);
+// // 배열 생성
+// var arr1 = [];
+// var arr2 = ["shin","lee"];
+// var arr3 = new Array();
+// var arr4 = new Array(1,2,3,4);
 
-console.log(arr1);
-console.log(arr2);
-console.log(arr3);
-console.log(arr4);
+// console.log(arr1);
+// console.log(arr2);
+// console.log(arr3);
+// console.log(arr4);
 
-// 배열 출력
-console.log(arr2[0]);
+// // 배열 출력
+// console.log(arr2[0]);
 
-// 배열 개수
-console.log(arr2.length);
+// // 배열 개수
+// console.log(arr2.length);
 
-// 배열 내 데이터 추가
-console.log(arr2.push(arr1));
-console.log(arr2);
+// // 배열 내 데이터 추가
+// console.log(arr2.push(arr1));
+// console.log(arr2);
 
 // 배열이란?
 // 비원시 자료형으로 서로 연관된 많은 데이터를 정리하여 담아두는 곳!
@@ -33,6 +33,6 @@ console.log(arr2);
 // 배열 내 데이터 추가 : 배열명.push(값)
 
 // 실습 1 : 자신의 개인정보를 저장할 빈 배열 만들기!
-// 실습 2 : 빈 배열에 이름, 나이, 직업유무, 먹었던 점심 메뉴, 시력을 추가해보자!
+// 실습 2 : 빈 배열에 이름, 나이, 직업유무, 점심 메뉴, 시력을 추가해보자!
 // → 점심 메뉴는 객체로, 시력은 배열로!
 // 실습 3 : 배열 요소에 null, undefined, NaN가 추가되는지 확인해보기!
---
+++ src/2주차_생활코딩/2주차_6_repetitive_2.js
@@ -0,0 +1,7 @@
+document.write("<div>6장 반복문 활용</div>");
+
+// document.querySelectorAll(태그); 
+// HTML에 존재하는 해당 태그 모두를 노드 리스트로 반환!
+
+// 실습 1 : .querySelector('')와 .querySelectorAll('') 차이!
+// 실습 2 : 프로덕트 코드에서 버튼 클릭 시 <a>태그 내 글자색 바꿔보기
