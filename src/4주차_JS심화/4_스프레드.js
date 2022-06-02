f5117a80038549b84ee4768c2662c3ad5eba48c8
syhan7516 <syhan7516@naver.com>
2022-06-02 18:26:45 +0900

    스프레드 연산자 수정

--- src/4주차_JS심화/4_스프레드.js
+++ src/4주차_JS심화/4_스프레드.js
@@ -1,38 +1,38 @@
 document.write("<div>4장 스프레드</div>");
 
-// 스프레트 연산자 할당 객체 예시
-const bibimbab = {
-    base1 : 'rice',
-    base2 : 'vegetable',
-}
+// // 스프레트 연산자 할당 객체 예시
+// const bibimbab = {
+//     base1 : 'rice',
+//     base2 : 'vegetable',
+// }
 
-const bulgogiBibimbab = {
-    base1 : 'rice',
-    base2 : 'vegetable',
-    plus : 'bulgogi',
-}
+// const bulgogiBibimbab = {
+//     base1 : 'rice',
+//     base2 : 'vegetable',
+//     plus : 'bulgogi',
+// }
 
-const rawbeefBibimbab = {
-    base1 : 'rice',
-    base2 : 'vegetable',
-    plus : 'rawbeefBibimbab',
-}
+// const rawbeefBibimbab = {
+//     base1 : 'rice',
+//     base2 : 'vegetable',
+//     plus : 'rawbeefBibimbab',
+// }
 
-const chamchiBibimbab = {
-    ...bibimbab,
-    plus : 'chamchi'
-}
+// const chamchiBibimbab = {
+//     ...bibimbab,
+//     plus : 'chamchi'
+// }
 
-console.log(chamchiBibimbab);
+// console.log(chamchiBibimbab);
 
-// 스프레트 연산자 할당 배열 예시
-const beef_water_Food = ['보쌈','찌개','수육'];
-const beef_fire_Food = ['제육','갈비','바베큐'];
-let all_beef_Food = [...beef_water_Food, ...beef_fire_Food];
-console.log(all_beef_Food);
+// // 스프레트 연산자 할당 배열 예시
+// const beef_water_Food = ['보쌈','찌개','수육'];
+// const beef_fire_Food = ['제육','갈비','바베큐'];
+// let all_beef_Food = [...beef_water_Food, ...beef_fire_Food];
+// console.log(all_beef_Food);
 
-all_beef_Food = [...beef_water_Food, ...beef_fire_Food, '물고기'];
-console.log(all_beef_Food);
+// all_beef_Food = [...beef_water_Food, ...beef_fire_Food, '물고기'];
+// console.log(all_beef_Food);
 
 // 스프레드 연산자 ?
 // : 중복되는 값을 편리하게 불러오기 가능 !
