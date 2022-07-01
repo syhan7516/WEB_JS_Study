d3d821f81d24dae7551c0a7fe23fd5f32025fc72
syhan7516 <syhan7516@naver.com>
2022-07-02 01:07:28 +0900

    전체적인 내용 추가

---
+++ src/4주차_JS심화/0_Truthy&Falsy.js
@@ -0,0 +1,34 @@
+document.write("<div>0장 Truthy&Falsy</div>");
+
+// Truthy & Falsy ?
+// Truthy : 참이아닌 참같은 값
+// Falsy : 거짓이아닌 거짓같은 값
+
+// Truthy 종류 ?
+// : 숫자, 문자, Infinity, [], {}
+
+// Falsy 종류 ?
+// : 0, -0, null, undefined, 값이 할당되지않은 변수, NaN, " " 
+
+// Truthy & Falsy 활용 방법 ?
+// : 객체인지 아닌지에 대한 결과!
+// const func = (likelion) => {
+//     if(likelion)
+//     {
+//         return "객체 X"
+//     }
+//     return likelion.insta;
+// }
+
+// let likelion;
+
+// result = func(likelion);
+
+// console.log(result);
+
+// ---------------------------------------------------------
+// 실습 1 : 조건문으로 Truthy 인지 Falsy 인지 console 찍어보기 !
+// 실습 2 : Truthy & Falsy 활용 방법 재작성 해보기 !
+
+// ---------------------------------------------------------
+//  1. 실습 정답
